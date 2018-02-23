3159f6d499cbea9b270f194062be6dfd986cbfbf
Matt Kemp <matt@mattikus.com>
2018-02-23 00:19:23 +0000

    I TYP GUD

--- lita/handlers/responses.rb
+++ lita/handlers/responses.rb
@@ -2,7 +2,7 @@ module Lita
   module Handlers
     class Responses < Handler
       # Make sure people know who QB is.
-      route(/:rotating_light:|who is qb/i, command: false) do |repsonse|
+      route(/:rotating_light:|who is qb/i, command: false) do |response|
         response.reply("*I AM QB*")
       end
     end
